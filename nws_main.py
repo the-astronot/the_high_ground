d5e964473b0a59445a9e93248c783828e2f39296
Jormungandr1105 <40341744+Jormungandr1105@users.noreply.github.com>
2019-11-03 04:06:24 -0500

    working server side script

--- nws_main.py
+++ nws_main.py
@@ -4,9 +4,10 @@ This script gets data from the National Weather Service and sends it to the data
 Author: Max Marshall
 Hack RPI 2019
 """
-from dependencies import lxml
-from dependencies import firebase_admin
-from dependencies import requests
+import firebase_admin
+from firebase_admin import firestore
+import requests
+import time
 from lxml import html
 
 
@@ -15,13 +16,20 @@ def get_cities(city_ref):
     docs = city_ref.stream()
 
     for doc in docs:
-        print(u'{} => {}'.format(doc.id, doc.to_dict()))
         cities_data.append((doc.id, doc.to_dict()))
     return cities_data
 
 
 def get_weather(city_data):
-    url = "https://forecast.weather.gov/MapClick.php?lat=18.464&lon=-66.1128".format()
+    for item in city_data[1]:
+        if item == "Latitude":
+            lat = city_data[1]["Latitude"]
+        elif item == "Longitude":
+            lon = city_data[1]["Longitude"]
+        elif item == "Flood":
+            flood = city_data[1]["Flood"]
+
+    url = "https://forecast.weather.gov/MapClick.php?lat={}&lon=-{}".format(lat, lon)
     page = requests.get(url)
     tree = html.fromstring(page.content)
 
@@ -29,28 +37,41 @@ def get_weather(city_data):
     for warning in warnings:
         if "Flood" in warning:
             print("True")
+            flood = True
+        else:
+            flood = False
+
+    city_data[1]["Flood"] = flood
+    new_city_data = city_data
 
     return new_city_data
 
 
 def send_weather(c_data):
-    pass
+    for item in c_data:
+        new_data_ref = db.collection(u'{}'.format(database)).document(u'{}'.format(item[0]))
+        new_data = dict()
+        for dict_item in item[1]:
+            new_data[u'{}'.format(dict_item)] = u'{}'.format(item[1][dict_item])
+        new_data_ref.set(new_data)
 
 
 cities_data = []
 new_cities_data = []
+database = "flood alerts"
 
 
 if __name__ == '__main__':
-    cred = firebase_admin.credentials.ApplicationDefault()
-    firebase_admin.initialize_app(cred, {
-        'projectId': "intricate-yew-257819"
-    })
+    cred = firebase_admin.credentials.Certificate("..\\the_high_ground\\keys\\"
+                                                  "intricate-yew-257819-firebase-adminsdk-20d4l-42b875fcc5.json")
+    firebase_admin.initialize_app(cred)
     db = firebase_admin.firestore.client()
 
     for x in range(5):
-        city_ref = db.collection(u'flood alerts')
+        city_ref = db.collection(u'{}'.format(database))
         cities_data = get_cities(city_ref)
         for city_data in cities_data:
             new_city_data = get_weather(city_data)
+            new_cities_data.append(new_city_data)
         send_weather(new_cities_data)
+        time.sleep(5)
